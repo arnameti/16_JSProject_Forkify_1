bf04d1738fc51b58ad64dcecec5453a2f7bd5659
Arnor Ameti <arnameti@gmail.com>
2022-07-05 17:43:43 +0200

    forkify base settings

---
+++ src/js/controller.js
@@ -0,0 +1,7 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import icons from "url:../img/icons.svg";
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
---
+++ src/js/views/recipeView.js
@@ -0,0 +1,3 @@
+// import icons from '../img/icons.svg'; // parcel 1
+import icons from "url:../../img/icons.svg"; // parcel 2
+import { Fraction } from "fractional";
